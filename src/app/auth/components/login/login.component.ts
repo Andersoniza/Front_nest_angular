d7b34b113af39d797aa42fb16610f2b46c192294
Andersoniza <ajc.iza@yavirac.edu.ec>
2024-11-13 23:21:37 -0500

    Clase13Front

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { error } from 'console';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  private authService = inject(AuthService)
+
+  loginForm = new FormGroup({
+    email: new FormControl("", [Validators.email, Validators.required]),//valores q quedan por defecto
+    password: new FormControl("", Validators.required)//igual por defecto
+  })
+
+  funIngresar() {
+    this.authService.loginConNest(this.loginForm.value).subscribe(
+      (res) => {
+        console.log(res)
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+    //alert("Ingresando...")
+  }
 
 }
